d6db2befc3527cbb1b46c0f2fc2e6bd25cbf9ba2
Irwin <irwinchan@me.com>
2015-08-22 10:39:23 -0400

    Initial commit. Create intial CRM, Contact, and Rolodex classes from tutorial.

---
+++ crm.rb
@@ -0,0 +1,95 @@
+require_relative 'rolodex.rb'
+
+class CRM
+  attr_accessor :title
+
+  def self.run(name)
+    crm = new(name)
+    crm.main_menu
+  end
+
+  def initialize(name)
+    @title = name
+    @rolodex = Rolodex.new
+  end
+
+  # This is what attr_accessor does:
+
+  # def title
+  #   @title
+  # end
+
+  # def title=(new_title)
+  #   @title = new_title
+  # end
+
+  def print_main_menu
+    puts "1. Add a contact"
+    puts "2. Modify a contact"
+    puts "3. Display all contacts"
+    puts "4. Display contact"
+    puts "5. Display contact attribute"
+    puts "6. Delete a contact"
+    puts "7. Exit"
+  end
+
+  def main_menu
+    while true
+      print_main_menu
+      print "Choose an option: "
+      user_input = gets.chomp.to_i
+      break if user_input == 7
+      choose_option(user_input)
+    end
+  end
+
+  def choose_option(input)
+    # input = user_input
+    case input
+    when 1 then add_contact
+    when 2 then modify_contact
+    when 3 then display_all_contacts
+    when 4 then display_contact
+    when 5 then display_contact_attribute
+    when 6 then delete_contact
+    when 7 then exit
+    else
+      puts "I'm sorry Dave, I'm afraid you can't do that."
+    end
+  end
+
+  def add_contact
+    print "First name: "
+    first_name = gets.chomp
+
+    print "Last name: "
+    last_name = gets.chomp
+
+    print "Email: "
+    email = gets.chomp
+
+    print "Notes: "
+    notes = gets.chomp
+
+    @rolodex.add_contact(first_name, last_name, email, notes)
+  end
+
+  def display_all_contacts
+    @rolodex.all.each do |contact|
+      puts "#{contact.id}: #{contact.full_name} / #{contact.email}"
+    end
+  end
+
+end
+
+
+CRM.run("Bitmaker Labs CRM")
+
+# my_crm = CRM.new("Bitmaker Labs CRM")
+# my_crm.main_menu
+
+
+# my_crm.title # => "Bitmaker Labs CRM"
+# my_crm.title = "Something else"
+
+
